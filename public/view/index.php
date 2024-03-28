2065a45711ae07a9ed0f2a2547a7eb8c24962052
Zinnur41 <zagidullin_zin@mail.ru>
2024-03-28 17:10:48 +0300

    some fixes

--- public/view/index.php
+++ public/view/index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <style>
-    form {
+    form, h1 {
         text-align: center;
     }
     label {
@@ -17,12 +17,22 @@
     }
 </style>
 <body>
+    <h1>Регистрация</h1>
     <form action="/register" method="post">
         <label for="_email">Email</label>
         <input type="text" name="email" id="_email">
         <label for="_password">Пароль</label>
-        <input type="text" name="password" id="_password"><br>
-        <input type="submit" value="Зарегистрироваться">
+        <input type="password" name="password" id="_password"><br>
+        <button class="btn btn-primary" type="submit">Зарегистрироваться</button>
+    </form>
+<br>
+    <h1>Вход</h1>
+    <form action="/authorize" method="post">
+        <label for="__email">Email</label>
+        <input type="text" name="email" id="__email">
+        <label for="__password">Пароль</label>
+        <input type="password" name="password" id="__password"><br>
+        <button class="btn btn-success" type="submit">Войти</button>
     </form>
 </body>
 </html>
