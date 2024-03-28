35660951fbc7ebd33539900c009b412817d82f48
Zinnur41 <zagidullin_zin@mail.ru>
2024-03-28 17:12:07 +0300

    add validate token file

---
+++ src/operations/feed.php
@@ -0,0 +1,12 @@
+<?php
+
+use app\Service\UserService;
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../src/Database/connect.php';
+
+/** @var $database */
+$user = new UserService($database);
+
+$token = $_POST['token'];
+print_r($user->feed($token));
